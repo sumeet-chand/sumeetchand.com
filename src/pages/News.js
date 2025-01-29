454dcbe0fc75910981e9602bfd5520381d2a16d9
Sumeet Singh <sumeet.singhji@outlook.com>
2025-01-29 21:26:18 +1100

    v1.0

--- src/pages/News.js
+++ src/pages/News.js
@@ -11,40 +11,21 @@ const News = () => {
           <div style={{ textAlign: 'center' }}>
           <h1>News</h1>
           </div>
-          {/* 
           <br />
           <h3>Delay with book "Cybernetics, Cyberware and Cyborgs"</h3>
-          <p>Date: 06/09/2023</p>
           <p>
             Sorry everyone. I'm looking for a forward (book introduction) from a person discussed
             within the book who lives overseas that I would like to personally visit first.
           </p>
           <p>
             I can't give an ETA as I'm busy with life (mostly volunteering in Sydney & studying in
-            my spare time so forgive me) but expect hopefully by end 2024.
+            my spare time so forgive me) but expect hopefully before 2026.
           </p>
           <br />
-          <h3>Let's make YouTube/Tik Tok vids on Cyborgs together!</h3>
-          <p>
-            I'm looking for non paid hobby film makers to I'm looking to start shooting a 10+ min
-            monthly ongoing documentary series on Cyborgs, Disability, Biomedical Engineering,
-            androids and Posthumanism. This will be casual filming in our careers free times.
-            Please share for me.
-          </p>
-          <p>
-            With guest speakers such as professors, doctors and cyborgs etc., with various
-            universities, hospitals, etc., we'll get to experience seeing body parts replaced
-            for prosthetics, androids, hospitals in surgery, chasing Steve Haworth's creations,
-            and more.
-          </p>
-          <p>
-            I cannot pay you (sad I know I'm not Scrooge McDuck) and we'll be using existing
-            filming gear (Hero 9-11, with Rode Mics, MacBook Pro's for editing etc.,).
-          </p> 
           <h3>Ongoing Biomedical Engineering Meetup</h3>
           <p>
-            You can regularly catch me at the Biomedical Engineering, Cyberware and Cyborgs
-            meetups group (Physically and online) here: <a href="https://www.meetup.com/biomedical-engineering-cyberware-and-cyborgs-group/">https://www.meetup.com/biomedical-engineering-cyberware-and-cyborgs-group/</a>
+            I host a ongoing Biomedical Engineering social group in Sydney, Australia. 
+            You can attend physically or online here: <a href="https://www.meetup.com/biomedical-engineering-cyberware-and-cyborgs-group/">https://www.meetup.com/biomedical-engineering-cyberware-and-cyborgs-group/</a>
           </p>
           <br />
           <h3>Ongoing Open Source projects</h3>
@@ -52,10 +33,9 @@ const News = () => {
             My Github is below, feel free to contribute anytime.
           </p>
           <p>
-            <a href="https://github.com/SumeetSinghJi">https://github.com/SumeetSinghJi</a>
+            <a href="https://github.com/sumeet-chand">https://github.com/sumeet-chand</a>
           </p>
           <br />
-          */}
         </div>
         <div className="column3"></div>
       </div>
