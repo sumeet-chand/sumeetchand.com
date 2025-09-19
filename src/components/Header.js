8e86151efe492fe2e4c8720ae31d25adc5dfb97c
Sumeet Singh <sumeet.singhji@outlook.com>
2025-09-19 20:36:07 +1000

    v1.0.0

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,9 @@ const Header = ({ onToggleDarkMode, darkMode }) => {
           <li><Link to="/books">Books</Link></li>
           <li><Link to="/biography">Biography</Link></li>
           <li><Link to="/gallery">Gallery</Link></li>
-          <li><Link to="/hope">HOPE</Link></li>
+          <li>
+            <a href="/hope" className="sparkle-link">HOPE</a>
+          </li>
           <li><Link to="/news">News</Link></li>
           <li><Link to="/contactus">Contact us</Link></li>
           <li><button onClick={onToggleDarkMode}>{darkMode ? '🌙' : '☀️'}</button></li>
